9f340a371975699096bc42b4c447d3d58ade762c
ous-radian-n <snine9801@gmail.com>
2023-08-17 18:04:16 +0900

    クリエイティブコモンズ適用

--- src/credit-footer.js
+++ src/credit-footer.js
@@ -1,6 +1,7 @@
 Vue.createApp({})
 	.component('footer-credit', {
-		template: `<p>百花繚乱研究所（©ラジアン(柏木主税) Radian-N 2021-{{nowYear}}）</p><p>
+		template: `<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="クリエイティブ・コモンズ・ライセンス" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br />この 作品 は <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">クリエイティブ・コモンズ 表示 - 非営利 4.0 国際 ライセンス</a>の下に提供されています。<br>
+		<p>百花繚乱研究所（©ラジアン(柏木主税) Radian-N 2021-{{nowYear}}）</p><p>
 			スペシャルサンクス：<a class="a-p" src="https://okauni.connpass.com/">岡山Unity勉強会</a>, <a class="a-p" src="https://sanuki-gamen.connpass.com/">讃岐GameN</a>
 		</p>`,
 		// https://qiita.com/ron-Qiita/items/17af311a506d08b7bf14
