fe894e8ad1d05c982d85727b0f7efc033476bc8e
ous-radian-n <snine9801@gmail.com>
2025-03-04 13:06:47 +0900

    キャラスト作品「93(クミ)編」ページ更新
    - 第2話②③公開
    - 小説リンクをMisskey.ioの連合ノートに変更

--- src/characterstory-about-component.js
+++ src/characterstory-about-component.js
@@ -15,11 +15,13 @@ const characterStoryAbout01Component = {
 			</p>
 			<slot name="info">
 				<h3 class="h3-wide">小説版(Misskey)連載中</h3><p>
-				<a href="https://misskey.numbertales-radiann.net/notes/9u6hpaj8di">「ある主従契約記録にて」（前日端ショートストーリー）</a><br>
-				<a href="https://misskey.numbertales-radiann.net/notes/9u6j6xmldw">小説版第０話「プロローグ」</a><br>
-				<a href="https://misskey.numbertales-radiann.net/notes/9ub3q94g7i">小説版第１話①「夢からの目覚め」</a><br>
-				<a href="https://misskey.numbertales-radiann.net/notes/9ujd28f38n">小説版第１話②「２人の夢追い人」</a><br>
-				<a href="https://misskey.numbertales-radiann.net/notes/9zjggaaubt">小説版第２話①「93(クミ)の野望(ユメ)」</a></p>
+				<a href="https://misskey.io/notes/9u6hpaj8gsti01os">「ある主従契約記録にて」（前日端ショートストーリー）</a><br>
+				<a href="https://misskey.io/notes/9u6j6xmlqint00f2">小説版第０話「プロローグ」</a><br>
+				<a href="https://misskey.io/notes/9ub3q94g114601zw">小説版第１話①「夢からの目覚め」</a><br>
+				<a href="https://misskey.io/notes/9ujd28f36gt500mw">小説版第１話②「２人の夢追い人」</a><br>
+				<a href="https://misskey.io/notes/9zjggaauq0l600d1">小説版第２話①「93(クミ)の野望(ユメ)」</a><br>
+				<a href="https://misskey.io/notes/a1n7et2fnrep02zx">小説版第２話②「九十九神計画」</a><br>
+				<a href="https://misskey.io/notes/a4qme67aneyd0bir">小説版第２話③「再挑戦」</a></p>
 				<h3 class="h3-wide">サウンドノベル連載準備中</h3><p>
 				<a class="a-commingsoon">Comming Soon...</a></p>
 			</slot>
